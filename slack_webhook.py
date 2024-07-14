5da83ca61be572880f7bc8a8ee4d5b5abcf7120e
CB-Azul <christianbognotazul@gmail.com>
2024-07-15 07:12:36 +0800

    updated class name to Slack, and variable to self

--- slack_webhook.py
+++ slack_webhook.py
@@ -1,25 +1,25 @@
 import requests
 import json
 
-class coLinkha:
-    def __init__(i, webhook_url, channel):
-        i.webhook_url = webhook_url
-        i.channel = channel
+class Slack:
+    def __init__(self, webhook_url, channel):
+        self.webhook_url = webhook_url
+        self.channel = channel
     
-    def send_message(i, message):
+    def send_message(self, message):
         headers = {'Content-Type': 'application/json'}
         data = {
-            'channel': i.channel,
+            'channel': self.channel,
             'text': message
         }
-        response = requests.post(i.webhook_url, headers=headers, data=json.dumps(data))
+        response = requests.post(self.webhook_url, headers=headers, data=json.dumps(data))
         
-        print(response.status_code)
+        return response.status_code
 
 
 webhook_url = ''
 channel = '#test'
-co_Likha = coLinkha(webhook_url, channel)
+co_Likha = Slack(webhook_url, channel)
 
 
 message = 'test message using python'
